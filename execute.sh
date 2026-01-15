9efcdfb97bf339b578fb8eb1fdb42635d3bb0ba5
Ёжики <66070922+Plensederu@users.noreply.github.com>
2026-01-15 14:40:09 +0100

    Replaced bash script with updated version

--- execute.sh
+++ execute.sh
@@ -2,23 +2,22 @@
 #SBATCH -A uppmax2025-3-5
 #SBATCH -M pelle
 #SBATCH -p gpu
-#SBATCH --gres=gpu:2
+#SBATCH --gres=gpu:1
 #SBATCH --mem=256G
 #SBATCH --mail-type=END
-#SBATCH -t 10:00:00
+#SBATCH -t 20:00:00
 
 # load module & package
 module load GCC/12.3.0
 
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES
 export PYTORCH_ALLOC_CONF=expandable_segments:True
+export ACCESS_TOKEN=#Replace with HF access token
 
 # setup conda environment and test whether GPU acces is TRUE
 source /proj/uppmax2025-3-5/private/max/anaconda3/etc/profile.d/conda.sh
 conda activate nlp-gpu
-python -c 'import torch; print(f" GPU available: {torch.cuda.is_available()}\n",
-                                f"Device name: {torch.cuda.get_device_name(0)}\n",
-                                f"Total memory: {torch.cuda.get_device_properties(0).total_memory / 1024**3:.2f} GB")'
+hf auth login --token "$ACCESS_TOKEN" --add-to-git-credential
 
 #CREATE TEMP PATHS
 python - << 'EOF'
@@ -27,75 +26,143 @@ nb = nbformat.read("RnD_Project.ipynb", as_version=4)
 
 with open("load-nmt.py", "w") as f:
     for cell in nb.cells:
-        if 'load NMT' in cell.get('metadata', {}).get('tags', []):
+        if 'NMT' in cell.get('metadata', {}).get('tags', []):
             f.write(cell.source + "\n")
 
 with open("load-llm.py", "w") as f:
     for cell in nb.cells:
-        if 'load LLM' in cell.get('metadata', {}).get('tags', []):
-            f.write(cell.source + "\n")
-
-with open("run-nmt.py", "w") as f:
-    for cell in nb.cells:
-        if 'run NMT' in cell.get('metadata', {}).get('tags', []):
-            f.write(cell.source + "\n")
-
-with open("run-llm.py", "w") as f:
-    for cell in nb.cells:
-        if 'run LLM' in cell.get('metadata', {}).get('tags', []):
+        if 'LLM' in cell.get('metadata', {}).get('tags', []):
             f.write(cell.source + "\n")
 
 with open("load-rm.py", "w") as f:
     for cell in nb.cells:
-        if 'load RM' in cell.get('metadata', {}).get('tags', []):
-            f.write(cell.source + "\n")
-
-with open("run-rm.py", "w") as f:
-    for cell in nb.cells:
-        if 'run RM' in cell.get('metadata', {}).get('tags', []):
+        if 'RM' in cell.get('metadata', {}).get('tags', []):
             f.write(cell.source + "\n")
 
 with open("get-bleu.py", "w") as f:
     for cell in nb.cells:
-        if 'get BLEU' in cell.get('metadata', {}).get('tags', []):
+        if 'BLEU' in cell.get('metadata', {}).get('tags', []):
+            f.write(cell.source + "\n")
+
+with open("make-plot.py", "w") as f:
+    for cell in nb.cells:
+        if 'PLOT' in cell.get('metadata', {}).get('tags', []):
+            f.write(cell.source + "\n")
+
+with open("make-csv.py", "w") as f:
+    for cell in nb.cells:
+        if 'CSV' in cell.get('metadata', {}).get('tags', []):
+            f.write(cell.source + "\n")
+
+with open("clean-noise.py", "w") as f:
+    for cell in nb.cells:
+        if 'NOISE' in cell.get('metadata', {}).get('tags', []):
+            f.write(cell.source + "\n")
+
+with open("select-sents.py", "w") as f:
+    for cell in nb.cells:
+        if 'SELECT' in cell.get('metadata', {}).get('tags', []):
+            f.write(cell.source + "\n")
+
+with open("mask-words.py", "w") as f:
+    for cell in nb.cells:
+        if 'MASK' in cell.get('metadata', {}).get('tags', []):
+            f.write(cell.source + "\n")
+
+with open("get-total.py", "w") as f:
+    for cell in nb.cells:
+        if 'MASK' in cell.get('metadata', {}).get('tags', []):
             f.write(cell.source + "\n")
 EOF
 
-                        #|‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|#
-#TEMP PATHS             #|   Pre-masking   |   Post-masking   |#
-L_NMT='load-nmt.py'     #|   1/1 Done      |   ---            |#
-L_LLM='load-llm.py'     #|   1/1 Done      |   ---            |#
-R_NMT='run-nmt.py'      #|   1/1 Done      |   0/1 Done       |#
-R_LLM='run-llm.py'      #|   9/9 Done      |   0/9 Done       |#
-L_RM='load-rm.py'       #|   0/1 Done      |   ---            |#
-R_RM='run-rm.py'        #|   0/2 Done      |   0/2 Done       |#
-BLEU='get-bleu.py'      #|   2/2 Done      |   0/2 Done       |#
-                        #|____________________________________|#
+                            #|‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|#
+#TEMP PATHS                 #|   Pre-masking   |   Post-masking   |#
+NMT='load-nmt.py'           #|   1/1  Done     |   1/1  Done      |#
+LLM='load-llm.py'           #|   9/9  Done     |   9/9  Done      |#
+RM='load-rm.py'             #|   1/1  Done     |   1/1  Done      |#
+TOTAL='get-total.py'        #|   1/1  Done     |   1/1  Done      |#
+BLEU='get-bleu.py'          #|   2/2  Done     |   2/2  Done      |#
+CSV='make-csv.py'           #|   1/1  Done     |   1/1  Done      |#
+PLOT='make-plot.py'         #|   ---           |   1/1  Done      |#
+NOISE='clean-noise.py'      #|   1/1  Done     |   1/1  Done      |#
+SELECT='select-sents.py'    #|   2/2  Done     |   2/2  Done      |#
+MASK='mask-words.py'        #|   1/1  Done     |   ---            |#
+                            #|____________________________________|#
 
 #EXECUTE SCRIPTS
 #--------LOAD MODELS--------#
-python $L_NMT
-python $L_LLM
-
-#--------TRANSLATE--------#
-python $R_NMT
-for i in $(seq 0 8); do                                 # loops over temps
+python $NMT ''
+for i in $(seq 0 8); do                                 # loops over temps (CLI range is inclusive)
     if [[ $i -eq 0 ]]; then
         prfx_1=$(printf "%.2f" 0.01)                    # sets first iteration to 0.01 instead of 0.00
     else
         prfx_1=$(printf "%.2f" $(echo "$i*0.2" | bc))   # prefix 1 ranges from 0.2 up to 1.6; actual temps
     fi
     prfx_2=$(printf "%02d" $((i*2)))                    # prefix 2 ranges from 00 to 16; used for names
-    python $R_LLM $prfx_1 $prfx_2
+    python $LLM $prfx_1 $prfx_2 ''
+done
+
+#--------REMOVE NOISE--------#
+for i in $(seq 0 8); do
+    temp=$(printf "t%02d" $((i*2)))
+    python $NOISE $temp ''
+done
+
+#--------SELECT SENTENCES--------#
+for i in $(seq 0 8); do
+    temp=$(printf "t%02d" $((i*2)))
+    python $SELECT llm $temp ''
+done
+for i in $(seq 4); do
+    python $SELECT ref $i
 done
 
 #--------BLEU SCORE--------#
-python $BLEU nmt ''
+python $BLEU nmt '' ''
 for i in $(seq 0 8); do                                # loops over temps
-    prfx=$(printf "%02d" $((i*2)))                     # prefix same as prefix 2 in previous loop
-    python $BLEU llm $prfx
+    prfx=$(printf "t%02d_" $((i*2)))                   # prefix same as prefix 2 in previous loop
+    python $BLEU llm $prfx ''
 done
+python $CSV ''
 
 #--------REWARD MODEL--------#
-#python $L_RM
-#python $R_RM
+for i in $(seq 0 8); do
+    temp=$(printf "t%02d" $((i*2)))
+    python $RM $temp ''
+done
+python $TOTAL 
+
+#--------PERTURBATION--------#
+python $MASK
+python $NMT '-mask'
+for i in $(seq 0 8); do
+    if [[ $i -eq 0 ]]; then
+        prfx_1=$(printf "%.2f" 0.01)
+    else
+        prfx_1=$(printf "%.2f" $(echo "$i*0.2" | bc))
+    fi
+    prfx_2=$(printf "%02d" $((i*2)))
+    python $LLM $prfx_1 $prfx_2 '-mask'
+done
+for i in $(seq 0 8); do
+    temp=$(printf "t%02d" $((i*2)))
+    python $NOISE $temp '-mask'
+done
+for i in $(seq 0 8); do
+    temp=$(printf "t%02d" $((i*2)))
+    python $SELECT llm $temp '-mask'
+done
+python $BLEU nmt '' '-mask'
+for i in $(seq 0 8); do
+    prfx=$(printf "t%02d_" $((i*2)))
+    python $BLEU llm $prfx '-mask'
+done
+python $CSV '-mask'
+for i in $(seq 0 8); do
+    temp=$(printf "t%02d" $((i*2)))
+    python $RM $temp 'mask-'
+done
+python $TOTAL 'mask-'
+
+#--------PLOT BLEU--------#
+python $PLOT
